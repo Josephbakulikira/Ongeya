735005956577c76805f29136155850bbb22dd5c7
joseph <auctux@gmail.com>
2019-10-14 15:33:56 +0530

    Redux form 101

--- src/MainApp/reducers/RootReducer.js
+++ src/MainApp/reducers/RootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import TestReducer from "../Store/TestReducer";
 import postReducer from "../../component/PostRedux/postReducer";
+import { reducer as FormReducer} from "redux-form"
 
 const RootReducer = combineReducers({
+    form: FormReducer,  
     test: TestReducer,
     posts: postReducer
 })
--- src/component/PostList/Formular/PostForm.jsx
+++ src/component/PostList/Formular/PostForm.jsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react'
-import { Segment, Form, Button,  } from 'semantic-ui-react'
+import { Segment, Form, Button, Grid, Header, Input,  } from 'semantic-ui-react'
+import {reduxForm, Field} from 'redux-form'
 import { connect } from 'react-redux';
+import {composeValidators, combineValidators, isRequired, hasLengthGreaterThan} from 'revalidate';
 import { createPost, updatePost} from '../../PostRedux/PostActions'
 import cuid from 'cuid';
+import TextInput from '../../../MainApp/Form/TextInput';
+import TextArea from '../../../MainApp/Form/TextArea';
+import SelectInput from '../../../MainApp/Form/SelectInput';
+import DateInput from '../../../MainApp/Form/DateInput';
 
 
 const mapState = (state, ownProps) => {
   const postId = ownProps.match.params.id;
 
-  let post = {
-    title: '',
-    date: '',
-    city: '',
-    venue: '',
-    hostedBy: ''
-  }
+  let post = {};
   if(postId && state.posts.length > 0){
     post = state.posts.filter(post => post.id === postId)[0]
   }
 
   return {
-    post
+    initialValues: post
   }
 }
 
@@ -29,40 +29,54 @@ const actions = {
   updatePost
 }
 
+const validate = combineValidators({
+  title: isRequired({message: 'please enter the post title'}),
+  category: isRequired({message: 'the category is required'}),
+  description: composeValidators(
+    isRequired({message: 'please enter the description'}),
+    hasLengthGreaterThan(6)({message: 'the description need to be at least 7 letter or character'})
+  )(),
+  city: isRequired('city'),
+  venue: isRequired('address'),
+  date : isRequired('date')
+})
+
+const category = [
+    
+    {key: 'culture', text: 'Culture', value: 'culture'},
+    {key: 'film', text: 'Film', value: 'film'},
+    {key: 'news', text: 'News', value: 'news'},
+    {key: 'event', text: 'Event', value: 'event'},
+    {key: 'food', text: 'Food', value: 'food'},
+    {key: 'comedy', text: 'Comedy', value: 'comedy'},
+    {key: 'music', text: 'Music', value: 'music'},
+    {key: 'travel', text: 'Travel', value: 'travel'},
+    {key: 'party', text: 'Party', value: 'party'},
+    {key: 'anime', text: 'Anime', value: 'anime'},
+    {key: 'drinks', text: 'Drinks', value: 'drinks'},
+];
+
 class PostForm extends Component {
-  state = {
-    ...this.props.post
-  };
-  componentDidMount()
-  {
-    if(this.props.selectedPost !== null )
-    {
-      this.setState({
-        ...this.props.selectedPost
-      })
-    }
-  };
+  
 
-  handleSubmitOfForm = (event) => 
+  handleSubmitOfForm = values => 
   {
-    event.preventDefault();
-    if(this.state.id)
+   
+    if(this.props.initialValues.id)
     {
-
-      this.props.updatePost(this.state);
-      this.props.history.push(`/posts/${this.state.id}`)
+      this.props.updatePost(values);
+      this.props.history.push(`/posts/${this.props.initialValues.id}`)
     }else{
       const newPost = {
-        ...this.state, 
+        ...values, 
 
         id: cuid(),
-        hostPhotoURL: '/images/user.png'
+        hostPhotoURL: '/images/user.png',
+        hostedBy: 'Ghost'
       }
       this.props.createPost(newPost);
-      this.props.history.push(`/posts`)
+      this.props.history.push(`/posts/${newPost.id}`)
     }
-    
-    
   }
 
   
@@ -75,38 +89,43 @@ class PostForm extends Component {
   };
 
   render() {
-
-      const {title, city, date, venue, hostedBy} = this.state;
+        const {history, initialValues, invalid, submitting, pristine} = this.props
+      
         return (
-                  <Segment>
-                    <Form onSubmit={this.handleSubmitOfForm} autoComplete='off'>
-                      <Form.Field>
-                        <label>Post Title</label>
-                        <input name="title" onChange={this.handleInputChange} value={title}  placeholder="Title" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label> Date</label>
-                        <input name="date" onChange={this.handleInputChange} value={date}  type="date" placeholder=" Date" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>City</label>
-                        <input name="city" onChange={this.handleInputChange} value={city} placeholder="City event is taking place" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Venue</label>
-                        <input name="venue" onChange={this.handleInputChange} value={venue}  placeholder="Enter the Venue of the event" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Hosted By</label>
-                        <input name="hostedBy" onChange={this.handleInputChange} value={hostedBy} placeholder="Enter the name of person hosting" />
-                      </Form.Field>
-                      <Button positive type="submit">
+              <Grid>
+                <Grid.Column width = {10}>
+                <Segment>
+                <Header sub color='teal' content='Post Detail' /> 
+                    <Form onSubmit={this.props.handleSubmit(this.handleSubmitOfForm)} autoComplete='off'>
+                      <Field name='title' component={TextInput} placeholder='name your post '/>
+                      <Field name='description' component={TextArea} rows={4} placeholder='tell us about your post'/>
+                      <Field name='category' component={SelectInput} options={category}  placeholder='what is it about'/>
+
+                      <Header sub color='teal' content='Post Location Details'/>
+                      <Field name='venue' component={TextInput} placeholder='post address'/>
+                      <Field name='city' component={TextInput} placeholder='post town'/>
+                      <Field name='date' 
+                      component={DateInput} 
+                      dateFormat='dd LLL yyyy h:mm a' 
+                      showTimeSelect
+                      timeFormat='HH:mm'
+                      placeholder='post date'/>
+                      <Button positive type="submit" disabled={invalid || submitting || pristine}>
                         Submit
                       </Button>
-                      <Button onClick={this.props.history.goBack} type="button">Cancel</Button>
+                      <Button onClick={initialValues.id? () => history.push(`/posts/${initialValues.id}`) :() => history.push('/posts')}
+                               type="button">Cancel</Button>
                     </Form>
                   </Segment>
+                </Grid.Column>
+
+                <Grid.Column width={6}>
+                  <h1>ads, pub</h1>
+                </Grid.Column>
+
+              </Grid>
+                  
         )
     }
 }
-export default connect(mapState, actions)(PostForm);
+export default connect(mapState, actions)(reduxForm({form: 'postForm', validate})(PostForm));
